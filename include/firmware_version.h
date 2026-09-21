c049216e80db511df652e95f06f011968aca8ca2
Justin Moen <justmoen@gmail.com>
2026-09-20 22:33:41 -0500

    add ethernet

--- include/firmware_version.h
+++ include/firmware_version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define FIRMWARE_VERSION "tank-7e5f7f4"
+#define FIRMWARE_VERSION "tank-f6bfab6"
 #define FIRMWARE_VARIANT "tank"
-#define FIRMWARE_GIT_SHA "7e5f7f4"
+#define FIRMWARE_GIT_SHA "f6bfab6"
