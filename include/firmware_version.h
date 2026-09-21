a7c64eddceb547a76ec5794c76ce6591280302ec
Justin Moen <justmoen@gmail.com>
2026-09-20 22:19:12 -0500

    platform

--- include/firmware_version.h
+++ include/firmware_version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define FIRMWARE_VERSION "tank-f446fca"
+#define FIRMWARE_VERSION "tank-763dce3"
 #define FIRMWARE_VARIANT "tank"
-#define FIRMWARE_GIT_SHA "f446fca"
+#define FIRMWARE_GIT_SHA "763dce3"
